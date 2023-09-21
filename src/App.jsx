2c0b52aa7b1f3edf412ddb58ba8aa6ebd2ba970c
Appaz <matias2007play4@gmail.com>
2023-09-21 01:56:16 -0300

    Progreso

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,24 @@ function App() {
   
   return (
     <>
+    <h1 className='Titulo-centrado'><a href="https://la35.net/assets/files/practicas/proyecto-p5.pdf" target='_blank'><u>Formulario</u></a></h1>
     <div id="cambio">
+      <button><a href="/navbar">Navbar</a></button>
       <button><a href="/random">Random</a></button>
       <button><a href="/clock">Reloj</a></button>
       <button><a href="/stopwatch">Cronometro</a></button>
       <button><a href="/tabla">Tabla</a></button>
+      <button><a href="/futbol">Futbol</a></button>
       <button><a href="/pokemon">Pokemon</a></button>
       <button><a href="/todos">Tareas</a></button>
       <button><a href="/blog">Blog</a></button>
       <button><a href="/ciphers">Cifrado</a></button>
       <button><a href="/trivia">Trivia</a></button>
       <button><a href="/contador">Contador</a></button>
-      <button><a href="/futbol">Futbol</a></button>
       <button><a href="/galeria">Galeria</a></button>
       <button><a href="/toggle">Render condicional</a></button>
       <button><a href="/countdown">Cuenta regresiva</a></button>
-      <button><a href="/unform">Un formulario</a></button>
-      <button><a href="/navbar">Navbar</a></button>        
+      <button><a href="/unform">Un formulario</a></button>        
     </div>
     </>
   )
