d39059206525a897b9b0b89b3d4552245b26be73
Appaz <matias2007play4@gmail.com>
2023-09-27 17:40:37 -0300

    Arreglos

--- Ayuda-TP-React/src/App.jsx
+++ Ayuda-TP-React/src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 function App() {
   const [cosas, setCosas] = useState([]);
   const [nuevaCosa, setNuevaCosa] = useState('');
+  {/*Para resetear el array cosas, accedes desde Ctrl + Mayus + c hasta Application, a localStorage, click derecho, y delete*/}
 
   useEffect(() => {
     let cosasGuardadas = JSON.parse(localStorage.getItem('cosas'));
@@ -20,22 +21,24 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Nuevo</h1>  
+      <h1 style={{color: 'Black'}}><u>Nuevo</u></h1>  
       <form onSubmit={handleSubmit}>
         <input 
           value={nuevaCosa}
           onChange={e => setNuevaCosa(e.target.value)}
           type="text"
+          style={{fontSize: '16px'}}
         />
         <input 
           type="submit"
           value="Agregar"
+          style={{fontSize: '16px'}}
         />
       </form>
-      <pre>[
+      <pre style={{fontSize: '17px'}}>[
         {cosas.map((c,i) => <span key={i}> {c} </span>)}
       ]</pre>
-      <a href="/lista">Ir a pagina de lista</a>
+      <a href="/lista" style={{fontSize: '16px'}}>Ir a pagina de lista</a>
     </div>
   );
 }
---
+++ Ayuda-TP-React/src/componentes/Lista.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+function Lista() {
+  return (
+    <ul className='Lista'>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
+};
+
+export default Lista
